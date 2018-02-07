edfdabb95a7b20f4dab54afaa7834a148fb30e03
ILGSupreme <Samuel_padillalara@hotmail.com>
2018-02-07 13:55:22 +0100

    Fixed lib issues and made changes into CMakeLists.txt.

--- Adon/Adem/src/helloworld.cpp
+++ Adon/Adem/src/helloworld.cpp
@@ -1,6 +1,20 @@
 #include "helloworld.h"
+#include <xercesc\util\PlatformUtils.hpp>
+
+using namespace xercesc;
 
 int testlib()
 {
+	try {
+		XMLPlatformUtils::Initialize();
+	}
+	catch (const XMLException& toCatch) {
+		// Do your failure processing here
+		return 1;
+	}
+
+	// Do your actual work with Xerces-C++ here.
+
+	XMLPlatformUtils::Terminate();
 	return 1;
 }
