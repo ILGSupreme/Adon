fe47b1b71a0ad1774e022942e910e692734892e2
ILGSupreme <Samuel_padillalara@hotmail.com>
2018-03-07 19:37:06 +0100

    git working branch, testing

---
+++ Adon/Adwin/src/adwin.h
@@ -0,0 +1,13 @@
+#ifndef ADWIN_HEADER
+#define ADWIN_HEADER
+#include <AdWin_export.h>
+
+namespace Adon
+{
+	namespace AdWin
+	{
+		ADWIN_EXPORT void testfunction();
+	}
+}
+
+#endif // !ADWIN_HEADER
---
+++ Adon/Adwin/test/src/main.cpp
@@ -0,0 +1,6 @@
+#include <AdWin.h>
+
+int main(int argc, char **argv)
+{
+	Adon::AdWin::testfunction();
+}
